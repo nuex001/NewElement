59f83c1b7d9cea71d15de93363bc5ac779e68881
Kristian Rahnev <mrchreesa@gmail.com>
2023-05-02 09:38:07 +0100

    recaptcha

--- components/ApplyComponent.tsx
+++ components/ApplyComponent.tsx
@@ -1,9 +1,5 @@
-import Link from "next/link";
-import React, { useRef, useState, useCallback } from "react";
-import bg from "../assets/background1.jpg";
+import React, { useState } from "react";
 import axios from "axios";
-import ReCAPTCHA from "react-google-recaptcha";
-import { log } from "console";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 
 const ApplyComponent = () => {
@@ -30,8 +26,6 @@ const ApplyComponent = () => {
 
   const { executeRecaptcha } = useGoogleReCaptcha();
 
-  const captchaRef = useRef(null as any);
-
   const isValidEmail = (email: string) => {
     const re =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -49,18 +43,18 @@ const ApplyComponent = () => {
     const emailValue = formValues.email.trim();
     let formIsValid = true;
 
-    if (formValues.website === "") {
-      setErrorMessageWebsite("Website is required");
-      setErrorWebsite(true);
-      formIsValid = false;
-    } else if (!isValidWebsite(formValues.website)) {
-      setErrorMessageEmail("Provide a valid website");
-      setErrorEmail(true);
-      formIsValid = false;
-    } else {
-      setErrorWebsite(false);
-      setFormWebsite(formValues.website);
-    }
+    // if (formValues.website === "") {
+    //   setErrorMessageWebsite("Website is required");
+    //   setErrorWebsite(true);
+    //   formIsValid = false;
+    // } else if (!isValidWebsite(formValues.website)) {
+    //   setErrorMessageEmail("Provide a valid website");
+    //   setErrorEmail(true);
+    //   formIsValid = false;
+    // } else {
+    //   setErrorWebsite(false);
+    //   setFormWebsite(formValues.website);
+    // }
 
     if (formValues.email === "") {
       setErrorMessageEmail("Email is required");
@@ -92,45 +86,27 @@ const ApplyComponent = () => {
     setIsOpenModal(false);
   };
 
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      borderRadius: "25px",
-      transform: "translate(-50%, -50%)",
-    },
-  };
-
-  // const handleSumitForm = useCallback(
-  //   (e: any) => {
-  //     e.preventDefault();
-  //     if (!executeRecaptcha) {
-  //       console.log("Execute recaptcha not yet available");
-  //       return;
-  //     }
-  //     executeRecaptcha("enquiryFormSubmit").then((gReCaptchaToken) => {
-  //       console.log(gReCaptchaToken, "response Google reCaptcha server");
-  //       handleSubmit(e, gReCaptchaToken);
-  //     });
+  // const customStyles = {
+  //   content: {
+  //     top: "50%",
+  //     left: "50%",
+  //     right: "auto",
+  //     bottom: "auto",
+  //     marginRight: "-50%",
+  //     borderRadius: "25px",
+  //     transform: "translate(-50%, -50%)",
   //   },
-  //   [executeRecaptcha]
-  // );
+  // };
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    // const token = captchaRef?.current?.getValue();
-    // captchaRef.current?.reset();
     if (!executeRecaptcha) {
       console.log("Execute recaptcha not yet available");
       return;
     }
     let token = await executeRecaptcha("enquiryFormSubmit").then(
       (gReCaptchaToken) => {
-        // console.log(gReCaptchaToken, "response Google reCaptcha server");
         return gReCaptchaToken;
       }
     );
@@ -145,7 +121,6 @@ const ApplyComponent = () => {
     if (validateInputs()) {
       console.log("Verification passed!");
       setLoading(true);
-      console.log(data);
 
       axios
         .post(`/api/apply`, data)
@@ -161,13 +136,12 @@ const ApplyComponent = () => {
               "Thank you for your interest in New Elements! We will be in touch soon."
             );
           } else {
-            console.log(response);
+            alert("Sorry, something went wrong. Please try again.");
           }
         })
         .catch((err) => console.log(err));
     } else {
       setLoading(false);
-
       console.log("Verification failed.");
     }
   };
@@ -181,7 +155,7 @@ const ApplyComponent = () => {
     <div
       className={`flex w-screen  h-screen  items-center justify-center bg-center bg-no-repeat bg-cover bg-applyPattern`}
     >
-      <div className="flex mb-5 mt-24 w-[90%] md:w-[50vw] xl:w-[40vw] max-w-[1600px] flex-col items-center uppercase text-left text-green font-ibmPlex">
+      <div className="flex mb-5 w-[90%] md:w-[50vw] xl:w-[40vw] max-w-[1600px] flex-col items-center uppercase text-left text-green font-ibmPlex">
         <div className=" w-full  text-xs">
           <form id="contact-form" onSubmit={handleSubmit} method="POST">
             <h2 className="text-xs md:leading-[25px] w-5/6 md:w-full mb-12 ">
@@ -302,18 +276,31 @@ const ApplyComponent = () => {
                 {errorMessageInstagram}
               </div>
             </div>
-            {/* <div className="">
-              <ReCAPTCHA
-                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
-                ref={captchaRef}
-              />
-            </div> */}
+
             <button
               type="submit"
               className="bg-blue text-green font-xCompressed border border-green w-full uppercase tracking-[8px] mt-3 bg-white bg-opacity-20 hover:bg-opacity-40 py-[1.2vh] px-[7vh] z-2 text-2xl  "
             >
               {loading ? (
-                <div className="loader ease-linear rounded-full border-4 border-t-4 border-white h-6 w-6"></div>
+                <div role="status" className="flex justify-center">
+                  <svg
+                    aria-hidden="true"
+                    className="w-8 h-8 mr-2 text-gray-200  animate-spin dark:text-green fill-gray-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
               ) : (
                 "Submit"
               )}
--- pages/api/apply.tsx
+++ pages/api/apply.tsx
@@ -1,3 +1,7 @@
+// In Production
+// Change nodemailer email
+// Change reCaptcha account
+
 import { transporter, mailOptions } from "../../lib/nodemailer";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -23,14 +27,28 @@ const generateEmailContent = (data: any) => {
   // const htmlData = Object.entries(data).reduce((str, [key, val]) => {
   //   return (str += `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS[key]}</h3><p class="form-answer" align="left">${val}</p>`);
   // }, "");
-  const htmlData = `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS.website}</h3><p class="form-answer" align="left">${data.website}</p>
-  <h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS.email}</h3><p class="form-answer" align="left">${data.email}</p>
-  <h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS.twitter}</h3><p class="form-answer" align="left">${data.twitter}</p>
-  <h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS.instagram}</h3><p class="form-answer" align="left">${data.Instagram}</p>`;
+  const htmlData = `
+    <h3 class="form-heading" align="left">${
+      CONTACT_MESSAGE_FIELDS.email
+    }</h3><p class="form-answer" align="left">${data.email}</p>
+  <h3 class="form-heading" align="left">${
+    CONTACT_MESSAGE_FIELDS.instagram
+  }</h3><p class="form-answer" align="left">${data.instagram}</p>
 
+  ${
+    data.website
+      ? `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS.website}</h3><p class="form-answer" align="left">${data.website}</p>`
+      : ""
+  }
+   ${
+     data.twitter
+       ? `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS.twitter}</h3><p class="form-answer" align="left">${data.twitter}</p>`
+       : ""
+   }
+`;
   return {
     text: stringData,
-    html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>New Contact Message</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
+    html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>New Artist Application</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
   };
 };
 
@@ -58,10 +76,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       })
         .then((reCaptchaRes) => reCaptchaRes.json())
         .then((reCaptchaRes) => {
-          console.log(
-            reCaptchaRes.score,
-            "Response from Google reCatpcha verification API"
-          );
           if (reCaptchaRes?.score > 0.5) {
             try {
               transporter.sendMail({
@@ -70,8 +84,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                 subject: `Application by ${data.instagram}`,
               });
               res.status(201).json({ success: true });
+              res.end();
             } catch (error) {
-              console.log(error);
               res.status(400).json({ success: false, message: error });
             }
           } else {
@@ -87,29 +101,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         message: "Error submitting the enquiry form",
       });
     }
-
-    // const validateHuman = async (token: string): Promise<boolean> => {
-    //   const secret = process.env.NEXT_PUBLIC_RECAPTCHAV3_SECRET_KEY;
-    //   const response = await fetch(
-    //     `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
-    //     {
-    //       method: "POST",
-    //     }
-    //   );
-    //   const data = await response.json();
-    //   console.log(data);
-
-    //   return data;
-    // };
-
-    // const human = await validateHuman(data.token);
-    // console.log(human);
-
-    // if (!human) {
-    //   res.status(400);
-    //   res.json({ errors: ["Please, you're not fooling us, bot."] });
-    //   return;
-    // }
   }
 };
 export default handler;
