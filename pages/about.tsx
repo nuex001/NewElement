7001dfc474f66077e08ac8783fe05f9b54b4d0b7
Kristian Rahnev <mrchreesa@gmail.com>
2023-05-04 14:32:46 +0100

    responsive fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import AboutComponent from "../components/AboutComponent";
 
 const about = () => {
-  return (
-    <div className="flex h-screen justify-center items-center">
-      <AboutComponent />
-    </div>
-  );
+  return <AboutComponent />;
 };
 
 export default about;
