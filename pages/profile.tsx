a639fbd6e3cfeccf4a50856eee2ba3d29a259f85
Kristian Rahnev <mrchreesa@gmail.com>
2023-06-08 18:20:01 +0100

    set collection address

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -6,8 +6,8 @@ import { useRouter } from "next/router";
 type Props = {};
 
 const profile = (props: Props) => {
-  const { setAuthedProfile, authedProfile } = useAuthedProfile();
-  const router = useRouter();
+  // const { setAuthedProfile, authedProfile } = useAuthedProfile();
+  // const router = useRouter();
   // {
   //   !authedProfile ? router.push("/") : <ProfileComponent />;
   // }
